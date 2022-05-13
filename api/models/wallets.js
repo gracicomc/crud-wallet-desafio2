9e020600345c894f8bbe3cb358e21460ec217114
Graciela Parente <graciparentte@gmail.com>
2022-05-13 10:21:46 -0300

    style: format space

--- api/models/wallets.js
+++ api/models/wallets.js
@@ -1,25 +1,26 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class Wallets extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
+    class Wallets extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+        }
     }
-  }
-  Wallets.init({
-    name: DataTypes.STRING,
-    cpf: DataTypes.STRING,
-    birthdate: DataTypes.DATEONLY
-  }, {
-    sequelize,
-    modelName: 'Wallets',
-  });
-  return Wallets;
+    Wallets.init(
+        {
+            name: DataTypes.STRING,
+            cpf: DataTypes.STRING,
+            birthdate: DataTypes.DATEONLY,
+        },
+        {
+            sequelize,
+            modelName: 'Wallets',
+        }
+    );
+    return Wallets;
 };
