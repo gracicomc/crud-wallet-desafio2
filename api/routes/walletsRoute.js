d5367db63a74f969676255ca01197c141191c0f8
Graciela Parente <graciparentte@gmail.com>
2022-05-13 16:04:10 -0300

    feat: create POST wallet method

--- api/routes/walletsRoute.js
+++ api/routes/walletsRoute.js
@@ -1,8 +1,10 @@
 const { Router } = require('express');
+const { route } = require('express/lib/application');
 const WalletController = require('../controllers/WalletController');
 
 const router = Router();
 
+router.post('/wallet', WalletController.postWallet);
 router.get('/wallets', WalletController.getAllWallets);
 
 module.exports = router;
